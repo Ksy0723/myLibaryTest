c49a28164fe704a46abd145d9f3f73e1191d8c2f
ksy <seayoung0723@gmail.com>
2022-10-12 16:58:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mylibarytest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mylibarytest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ksy.mylibrary.ShowLibrary.Companion.lToast
+import com.ksy.mylibrary.ShowLibrary.Companion.sToast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sToast(this, "테스트 Toast")
+        lToast(this, "테스트 Toast")
+    }
+}
