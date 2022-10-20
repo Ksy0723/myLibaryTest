a9442c38fd72e61a2ea2bf70c30fe5a7c2a60986
ksy <seayoung0723@gmail.com>
2022-10-20 14:48:29 +0900

    테스트2

--- libraryTest/src/main/java/com/ksy/mylibrary/ShowLibrary.kt
+++ libraryTest/src/main/java/com/ksy/mylibrary/ShowLibrary.kt
@@ -6,8 +6,8 @@ import android.widget.Toast
 class ShowLibrary {
     companion object{
 
-        fun sToast(context: Context, msg : String) = Toast.makeText(context,msg,Toast.LENGTH_SHORT).show()
+        private fun sToast(context: Context, msg : String) = Toast.makeText(context,msg,Toast.LENGTH_SHORT).show()
 
-        fun lToast(context: Context, msg : String) = Toast.makeText(context,msg,Toast.LENGTH_LONG).show()
+        private fun lToast(context: Context, msg : String) = Toast.makeText(context,msg,Toast.LENGTH_LONG).show()
     }
 }
